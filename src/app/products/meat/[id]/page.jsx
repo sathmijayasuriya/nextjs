63c1fac909f4784388a9ef7d9297976619f8afef
sathmijayasuriya <sathmijayasuriya@gmail.com>
2025-01-02 21:34:41 +0530

    How to Link Navigate Between Pages & Dynamic Pages

--- src/app/products/meat/[id]/page.jsx
+++ src/app/products/meat/[id]/page.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-
+import Link from "next/link";
 export default function page({ params }) {
   return (
     <>
       <h1> meat item is {params.id}</h1>
+      <div>
+        <button style={{margin:"10px",padding:"7px"}}>
+          <Link href="/products/meat">
+          back to products
+          </Link>
+        </button>
+      </div>
     </>
   );
 }
