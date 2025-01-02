616a3d4950be298958be42260e45195bf634bd63
sathmijayasuriya <sathmijayasuriya@gmail.com>
2025-01-02 21:29:57 +0530

    How to Link Navigate Between Pages & Dynamic Pages

--- src/app/products/meat/page.jsx
+++ src/app/products/meat/page.jsx
@@ -1,13 +1,37 @@
 import React from "react";
 import Link from "next/link";
 export default function page() {
+  const meat = [
+    {
+      id: 1,
+      meatName: "chicken",
+    },
+    {
+      id: 2,
+      meatName: "eggs",
+    },
+    {
+      id: 3,
+      meatName: "sausages",
+    },
+  ];
+
   return (
     <>
       <h1> meat items</h1>
-      <ul>
+      {/* hard code */}
+      {/* <ul>
         <li><Link href="/products/meat/chicken">chicken</Link></li>
         <li><Link  href="/products/meat/eggs">eggs</Link></li>
         <li><Link  href="/products/meat/sausages">sausages</Link></li>
+      </ul> */}
+
+      <ul>
+        {meat.map((id) => {
+          return (
+            <Link href={`/products/meat/${id.meatName}`}><li>{id.meatName}</li></Link>
+          );
+        })}
       </ul>
     </>
   );
