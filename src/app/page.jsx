b8af6924c65f8494bdf4d4016ce18d36b7ef500d
sathmijayasuriya <sathmijayasuriya@gmail.com>
2025-01-02 22:37:31 +0530

    SS Styles CSS Modules & globals.css

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,13 +7,13 @@ export default function Home() {
     <div className={styles.page}>
       <h1>Next.js concepts</h1>
       <ul>
-        <li><a href="/">Home</a></li>
+        <li><a href="/" className={styles.link}> Home</a></li>
         <li><a href="/services"> Services</a></li>
         <li><Link href="/about">About</Link></li>
         <li><Link href="/products">Prodcuts</Link></li>
         <li><Link href="/">Contact Us</Link></li>
         {/* external sites open */}
-        <li><Link href = "https://nextjs.org/" target="_blank"> NextJS </Link></li>
+        <li><Link id={styles.specialId} href = "https://nextjs.org/" target="_blank"> NextJS </Link></li>
         <li><Link href="/products/meat">meat page</Link></li>
 
       </ul>
