42380e2620be8a51e7a8f1ce2d38984e1872ab2c
sathmijayasuriya <sathmijayasuriya@gmail.com>
2025-01-07 00:15:52 +0530

    Remote Images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'image.cdn2.seaart.me',
+            search: '**',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
 import mountain from '../../public/mountain.png';
+import logo from '../../public/nextjslogo.jpeg';
 export default function Home() {
 
   console.log("image ----------- ",mountain);
@@ -23,7 +24,14 @@ export default function Home() {
       <div style={{height:"50%"}}>
       {/* It automatically optimizes images by resizing and compressing them to deliver faster load times. 
       Next.js creates optimized images on demand and stores them temporarily in its server cache. */}
-      <Image alt="image optimization" style={{width:"100%"}} src={mountain}/>  
+      <Image alt="image optimization" style={{width:"100%",objectFit:"cover"}} src={mountain}/>  
+      <img style={{width:"50%"}} src={logo.src} alt="nextJS Logo" />
+      <Image
+      src="https://image.cdn2.seaart.me/2024-09-19/crlp9b5e878c73f1uflg/0304057175d2d0fea4795ebab810347c_high.webp"
+      alt="remote Image"
+      width={700}
+      height={900}
+    />
       </div>
     </div>
   );
